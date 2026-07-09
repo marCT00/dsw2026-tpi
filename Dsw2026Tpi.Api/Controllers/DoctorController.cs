76aa4986e36c4e2b4d87b37e6ad58719f823bfef
Francisco Vicente <franciscovicente@doc.frt.utn.edu.ar>
2026-07-09 20:13:01 -0300

    Se completa ejemplo de controlador con paginación.

--- Dsw2026Tpi.Api/Controllers/DoctorController.cs
+++ Dsw2026Tpi.Api/Controllers/DoctorController.cs
@@ -1,18 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dsw2026Tpi.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Dsw2026Tpi.Api.Controllers;
 
 [Route("doctors")]
 public class DoctorController : AppController
 {
-    public DoctorController()
+    private readonly IDoctorService _service;
+
+    public DoctorController(IDoctorService service)
     {
-        
+        _service = service;
     }
 
     [HttpGet]
     public async Task<IActionResult> GetAll([FromQuery]int pageSize, [FromQuery]int pageIndex, [FromQuery]string? name = null)
     {
-        return Ok("Hola Mundo!");
+        var doctors = await _service.GetAll(pageSize, pageIndex, name);
+        return Ok(doctors);
     }
 }

391c702f332d8d204d02129e04e2b3c7dda0eb84
Mar <ingmarsaave@gmail.com>
2026-08-03 18:53:51 -0300

    Se agrego clase de Rate Limiting

---
+++ Dsw2026Tpi.Api/Configurations/RateLimitingConfigurationExtentions.cs
@@ -0,0 +1,11 @@
+﻿namespace Dsw2026Tpi.Api.Configurations
+{
+    public class RateLimitingConfigurationExtentions
+    {
+        public int WindowMinutes { get; set; }
+        public int AdminLoginLimit { get; set; }
+        public int PatientLoginLimit { get; set; }
+        public int BookingLimit { get; set; }
+        public int GlobalLimit { get; set; }
+    }
+}

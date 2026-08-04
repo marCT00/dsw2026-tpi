bab82bcf51707663653f95ad0fcd3f556ed550ba
Mar <ingmarsaave@gmail.com>
2026-08-04 10:38:07 -0300

    Corrección Availability Model

--- Dsw2026Tpi.Application/Dtos/AvailabilityModel.cs
+++ Dsw2026Tpi.Application/Dtos/AvailabilityModel.cs
@@ -11,8 +11,5 @@ public static class AvailabilityModel
     public record Response(Guid Id, int Year, int Month, DayOfWeek DayOfWeek,
         TimeSpan StartTime, TimeSpan EndTime, Guid DoctorId);
 
-    public record SlotResponse(Guid Id, DateTime ScheduledDate, TimeSpan StartTime,
-        TimeSpan EndTime, TurnState State, Guid AvailabilityId); //M: esto no se usa, pero lo dejo por si acaso
-
     public record DoctorAvailabilityResponse(Guid Id, string Day, string StartTime, string EndTime);
 }

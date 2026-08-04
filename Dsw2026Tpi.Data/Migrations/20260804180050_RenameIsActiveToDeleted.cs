aa92d48df81620e00f09305ccaabafb1b4a9757d
Mar <ingmarsaave@gmail.com>
2026-08-04 15:06:34 -0300

    C19 - Migración IsActive -> Deleted

---
+++ Dsw2026Tpi.Data/Migrations/20260804180050_RenameIsActiveToDeleted.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dsw2026Tpi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameIsActiveToDeleted : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Specialities");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Doctors");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Specialities",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Doctors",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Specialities");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Doctors");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Specialities",
+                type: "bit",
+                nullable: false,
+                defaultValue: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Doctors",
+                type: "bit",
+                nullable: false,
+                defaultValue: true);
+        }
+    }
+}
